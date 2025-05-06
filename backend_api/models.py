fa48d3ec17cc71d0fa0618ba1e7c47114b062b6c
imssbienestar-oss <irving.hernandezimssb@gmail.com>
2025-05-06 09:25:25 -0600

    Update models.py

--- backend_api/models.py
+++ backend_api/models.py
@@ -18,7 +18,7 @@ class Doctor(Base):
     especialidad = Column(String(255), nullable=True)
     entidad = Column(String(100), nullable=True)
     clues_ssa = Column(String(100), nullable=True)
-    forma_notificacion_baja = Column(Text, nullable=True)
+    notificacion_baja = Column(Text, nullable=True)
     motivo_baja = Column(Text, nullable=True)
     fecha_extraccion = Column(String(100), nullable=True) # O TEXT si lo cambiaste en la DB
     fecha_notificacion = Column(Date, nullable=True) # O TEXT si lo cambiaste en la DB
@@ -34,7 +34,7 @@ class Doctor(Base):
     estrato = Column(String(100), nullable=True)
     acuerdo = Column(String(255), nullable=True) # Lo pongo como String por el 2024.0 que vimos, ajusta si es necesario
     # Añade aquí CUALQUIER OTRA COLUMNA que tengas en tu tabla 'doctores'
-
+    __table_args__ = {'extend_existing': True}
     # Nota: Añadí nullable=True a la mayoría asumiendo que pueden estar vacías (NULL).
     # Ajusta unique=True, index=True, nullable=True según la definición real de tu tabla.
     # Si cambiaste las columnas de fecha a TEXT en la base de datos, CAMBIA Date por Text o String aquí también.
@@ -47,3 +47,4 @@ class User(Base):
     username = Column(String(100), unique=True, index=True, nullable=False) # Nombre de usuario único
     hashed_password = Column(String(255), nullable=False) # Contraseña almacenada de forma segura (hash)
     # is_active = Column(Boolean, default=True) # Opcional: para desactivar usuarios    
+    __table_args__ = {'extend_existing': True}
