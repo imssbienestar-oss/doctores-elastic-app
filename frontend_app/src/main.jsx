c717039da1e75b5ac57f781c05be83f84f24540d
imssbienestar-oss <irving.hernandezimssb@gmail.com>
2025-05-07 17:05:51 -0600

    Update main.jsx

--- frontend_app/src/main.jsx
+++ frontend_app/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Importar BrowserRouter
+import { AuthProvider } from './contexts/AuthContext'; // Asegúrate que la ruta a tu AuthContext sea correcta
+import App from './App.jsx';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* Envolver con BrowserRouter para habilitar el enrutamiento */}
+      <AuthProvider> {/* Envolver con AuthProvider para proveer el contexto de autenticación */}
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
